dab6e72f91567b16db3b5504169d97f991ba2af3
hangocmy <hangocmy00@gmail.com>
2022-06-25 15:03:45 +0700

    action add voucher
    - Add to cart without login
    - Add to cart successfully
    - Add to favorite list without login
    - Add to favorite list successfully
    - Add to compare voucher

--- cypress/e2e/main/add-to-compare.js
+++ cypress/e2e/main/add-to-compare.js
@@ -2,12 +2,11 @@ import { modalDialog } from "../../pageObject/share/common-modal-dialog";
 import { mainPage } from "../../pageObject/mainPage";
 require("cypress-xpath");
 
-
-describe('Add to cart', () => {
+describe('Add to compare voucher', () => {
   beforeEach(() => {
     cy.login();
   });
-
+  
 
   it('View main page', () => {
     cy.logout();
@@ -16,23 +15,23 @@ describe('Add to cart', () => {
   });
 
 
-  it('Add to cart without login', () => {
+  it('Add to compare without login', () => {
     cy.logout();
     modalDialog
       .clickMainPage();
     
     mainPage
-      .clickVoucher()
-      .clickAddToCart()
+      .clickCompare(1)
+      .clickCompare(2)
       .shouldShowMessageAddToCart('Vui lòng đăng nhập');
   });
 
 
-  it('Add to cart successfully', () => {
+  it('Add to compare', () => {
     mainPage
-      .clickVoucher()
-      .clickAddToCart()
-      .shouldShowMessageAddToCart('Thêm sản phẩm vào giỏ hàng thành công');
+      .clickCompare(1)
+      .clickCompare(2)
+      .shouldShowMessageCompare('So sánh sản phẩm thành công');
   });
 
   afterEach(() => {
--- cypress/pageObject/mainPage.js
+++ cypress/pageObject/mainPage.js
@@ -2,6 +2,7 @@ export const mainPage = {
 
   BTN_ADD_TO_CART: "//button[@class='btn btn-fefault cart add-to-cart-each']",
   BTN_ADD_OVERLAY: '.pagination-data > :nth-child(1) > .product-image-wrapper > .single-products > .product-overlay > a',
+  BTN_FAVORITE: "(//a[contains(text(),'Ưa thích')])[3]",
 
   clickVoucher(){
     cy.get(this.BTN_ADD_OVERLAY).click({force: true});
@@ -13,10 +14,33 @@ export const mainPage = {
     return this;
   },
 
-  shouldShowErrorMessage(msg){
+  clickAddToFavorite(){
+    cy.xpath(this.BTN_FAVORITE).click();
+    return this;  
+  },
+
+  clickCompare(i){
+    let BTN_COMPARE = `(//a[contains(text(),'So sánh')])[${i}]`;
+    cy.xpath(BTN_COMPARE).click();
+    return this;  
+  },
+
+  shouldShowMessageAddToCart(msg){
     let LBL_SUCCESS= `.show-error-message-detail`;
     cy.get(LBL_SUCCESS).should('have.text', msg);
     return this;
-  }
+  },
+
+  shouldShowMessageAddToFavorite(msg){
+    let LBL_SUCCESS = "//div[@class='content']";
+    cy.xpath(LBL_SUCCESS).should('have.text', msg);
+    return this;
+  },
+
+  shouldShowMessageCompare(msg){
+    let LBL_SUCCESS = "//div[@class='content']";
+    cy.xpath(LBL_SUCCESS).should('have.text', msg);
+    return this;
+  },
 
 }
